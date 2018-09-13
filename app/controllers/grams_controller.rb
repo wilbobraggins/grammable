8fde22fdb890cb2f195b29d28a7e1bc09f6f6de1
wilbobraggins <wilbobraggins@gmail.com>
2018-09-13 06:55:55 +0000

    create

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -4,9 +4,17 @@ class GramsController < ApplicationController
   end
 
   def create
+    @gram = Gram.create(gram_params)
+    redirect_to root_path
   end
 
   def index
   end 
+
+  private
+
+  def gram_params
+    params.require(:gram).permit(:message)
+  end
 
 end
